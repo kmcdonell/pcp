e591251bf2f6319f73438cf99f24d9ac5cbae0b0
Ken McDonell <kenj@kenj.id.au>
2026-09-09 15:55:31 +1000

    More @coderabbitai cleanups for PR #2710

--- src/libpcp3/src/getopt.c
+++ src/libpcp3/src/getopt.c
@@ -804,119 +804,121 @@ __pmStartOptions(pmOptions *opts)
     if (opts->flags & PM_OPTFLAG_INIT)
 	return;
 
+    PM_LOCK(__pmLock_extcall);
     /* need to check for PCP_DEBUG first ... */
-    if ((value = getenv("PCP_DEBUG")) != NULL) {
+    if ((value = getenv("PCP_DEBUG")) != NULL) {	/* THREADSAFE */
 	__pmSetDebugFlag(opts, value);
 	if (pmDebugOptions.config)
 	    fprintf(stderr, "pmGetOptions: PCP_DEBUG=%s set from the environment\n", value);
     }
 
     /* then more or less in alphabetical order */
-    if ((value = getenv("PCP_ALIGN_TIME")) != NULL) {
+    if ((value = getenv("PCP_ALIGN_TIME")) != NULL) {	/* THREADSAFE */
 	__pmSetAlignment(opts, value);
 	if (pmDebugOptions.config)
 	    fprintf(stderr, "pmGetOptions: PCP_ALIGN_TIME=%s set from the environment\n", value);
     }
-    if ((value = getenv("PCP_ARCHIVE")) != NULL) {
+    if ((value = getenv("PCP_ARCHIVE")) != NULL) {	/* THREADSAFE */
 	__pmAddOptArchive(opts, value);
 	if (pmDebugOptions.config)
 	    fprintf(stderr, "pmGetOptions: PCP_ARCHIVE=%s set from the environment\n", value);
     }
-    if ((value = getenv("PCP_ARCHIVE_LIST")) != NULL) {
+    if ((value = getenv("PCP_ARCHIVE_LIST")) != NULL) {	/* THREADSAFE */
 	__pmAddOptArchiveList(opts, value);
 	if (pmDebugOptions.config)
 	    fprintf(stderr, "pmGetOptions: PCP_ARCHIVE_LIST=%s set from the environment\n", value);
     }
-    if ((value = getenv("PCP_FOLIO")) != NULL) {
+    if ((value = getenv("PCP_FOLIO")) != NULL) {	/* THREADSAFE */
 	__pmAddOptArchiveFolio(opts, value);
 	if (pmDebugOptions.config)
 	    fprintf(stderr, "pmGetOptions: PCP_FOLIO=%s set from the environment\n", value);
     }
-    if ((value = getenv("PCP_GUIMODE")) != NULL) {
+    if ((value = getenv("PCP_GUIMODE")) != NULL) {	/* THREADSAFE */
 	__pmSetGuiModeFlag(opts);
 	if (pmDebugOptions.config)
 	    fprintf(stderr, "pmGetOptions: PCP_GUIMODE=%s set from the environment\n", value);
     }
-    if ((value = getenv("PCP_HOST")) != NULL) {
+    if ((value = getenv("PCP_HOST")) != NULL) {	/* THREADSAFE */
 	__pmAddOptHost(opts, value);
 	if (pmDebugOptions.config)
 	    fprintf(stderr, "pmGetOptions: PCP_HOST=%s set from the environment\n", value);
     }
-    if ((value = getenv("PCP_HOST_LIST")) != NULL) {
+    if ((value = getenv("PCP_HOST_LIST")) != NULL) {	/* THREADSAFE */
 	__pmAddOptHostList(opts, value);
 	if (pmDebugOptions.config)
 	    fprintf(stderr, "pmGetOptions: PCP_HOST_LIST=%s set from the environment\n", value);
     }
-    if ((value = getenv("PCP_SPECLOCAL")) != NULL) {
+    if ((value = getenv("PCP_SPECLOCAL")) != NULL) {	/* THREADSAFE */
 	__pmSetLocalContextTable(opts, value);
 	if (pmDebugOptions.config)
 	    fprintf(stderr, "pmGetOptions: PCP_SPECLOCAL=%s set from the environment\n", value);
     }
-    if ((value = getenv("PCP_LOCALMODE")) != NULL) {
+    if ((value = getenv("PCP_LOCALMODE")) != NULL) {	/* THREADSAFE */
 	__pmSetLocalContextFlag(opts);
 	if (pmDebugOptions.config)
 	    fprintf(stderr, "pmGetOptions: PCP_LOCALMODE=%s set from the environment\n", value);
     }
-    if ((value = getenv("PCP_LOCALPMDA")) != NULL) {
+    if ((value = getenv("PCP_LOCALPMDA")) != NULL) {	/* THREADSAFE */
 	__pmSetLocalContextFlag(opts);
 	if (pmDebugOptions.config)
 	    fprintf(stderr, "pmGetOptions: PCP_LOCALPMDA=%s set from the environment\n", value);
     }
-    if ((value = getenv("PCP_NAMESPACE")) != NULL) {
+    if ((value = getenv("PCP_NAMESPACE")) != NULL) {	/* THREADSAFE */
 	__pmSetNameSpace(opts, value, 1);
 	if (pmDebugOptions.config)
 	    fprintf(stderr, "pmGetOptions: PCP_NAMESPACE=%s set from the environment\n", value);
     }
-    if ((value = getenv("PCP_UNIQNAMES")) != NULL) {
+    if ((value = getenv("PCP_UNIQNAMES")) != NULL) {	/* THREADSAFE */
 	__pmSetNameSpace(opts, value, 0);
 	if (pmDebugOptions.config)
 	    fprintf(stderr, "pmGetOptions: PCP_UNIQNAMES=%s set from the environment\n", value);
     }
-    if ((value = getenv("PCP_ORIGIN")) != NULL) {
+    if ((value = getenv("PCP_ORIGIN")) != NULL) {	/* THREADSAFE */
 	__pmSetOrigin(opts, value);
 	if (pmDebugOptions.config)
 	    fprintf(stderr, "pmGetOptions: PCP_ORIGIN=%s set from the environment\n", value);
     }
-    if ((value = getenv("ORIGIN_TIME")) != NULL) {
+    if ((value = getenv("ORIGIN_TIME")) != NULL) {	/* THREADSAFE */
 	__pmSetOrigin(opts, value);
 	if (pmDebugOptions.config)
-	    fprintf(stderr, "pmGetOptions: PCP_ORIGIN_TIME=%s set from the environment\n", value);
+	    fprintf(stderr, "pmGetOptions: ORIGIN_TIME=%s set from the environment\n", value);
     }
-    if ((value = getenv("PCP_GUIPORT")) != NULL) {
+    if ((value = getenv("PCP_GUIPORT")) != NULL) {	/* THREADSAFE */
 	__pmSetGuiPort(opts, value);
 	if (pmDebugOptions.config)
 	    fprintf(stderr, "pmGetOptions: PCP_GUIPORT=%s set from the environment\n", value);
     }
-    if ((value = getenv("PCP_START_TIME")) != NULL) {
+    if ((value = getenv("PCP_START_TIME")) != NULL) {	/* THREADSAFE */
 	__pmSetStartTime(opts, value);
 	if (pmDebugOptions.config)
 	    fprintf(stderr, "pmGetOptions: PCP_START_TIME=%s set from the environment\n", value);
     }
-    if ((value = getenv("PCP_SAMPLES")) != NULL) {
+    if ((value = getenv("PCP_SAMPLES")) != NULL) {	/* THREADSAFE */
 	__pmSetSampleCount(opts, value);
 	if (pmDebugOptions.config)
 	    fprintf(stderr, "pmGetOptions: PCP_SAMPLES=%s set from the environment\n", value);
     }
-    if ((value = getenv("PCP_FINISH_TIME")) != NULL) {
+    if ((value = getenv("PCP_FINISH_TIME")) != NULL) {	/* THREADSAFE */
 	__pmSetFinishTime(opts, value);
 	if (pmDebugOptions.config)
 	    fprintf(stderr, "pmGetOptions: PCP_FINISH_TIME=%s set from the environment\n", value);
     }
-    if ((value = getenv("PCP_INTERVAL")) != NULL) {
+    if ((value = getenv("PCP_INTERVAL")) != NULL) {	/* THREADSAFE */
 	__pmSetSampleInterval(opts, value);
 	if (pmDebugOptions.config)
 	    fprintf(stderr, "pmGetOptions: PCP_INTERVAL=%s set from the environment\n", value);
     }
-    if ((value = getenv("PCP_TIMEZONE")) != NULL) {
+    if ((value = getenv("PCP_TIMEZONE")) != NULL) {	/* THREADSAFE */
 	__pmSetTimeZone(opts, value);
 	if (pmDebugOptions.config)
 	    fprintf(stderr, "pmGetOptions: PCP_TIMEZONE=%s set from the environment\n", value);
     }
-    if ((value = getenv("PCP_HOSTZONE")) != NULL) {
+    if ((value = getenv("PCP_HOSTZONE")) != NULL) {	/* THREADSAFE */
 	__pmSetHostZone(opts);
 	if (pmDebugOptions.config)
 	    fprintf(stderr, "pmGetOptions: PCP_HOSTZONE=%s set from the environment\n", value);
     }
+    PM_UNLOCK(__pmLock_extcall);
 
     opts->flags |= PM_OPTFLAG_INIT;
 }
